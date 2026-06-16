615fe978c522a5d429934003e910484e7c96aa08
Gokul <vijigokul2006@gmail.com>
2026-06-16 15:28:31 +0530

    personal profile

---
+++ personalprofile.py
@@ -0,0 +1,16 @@
+name=input("Enter name:")
+age=int(input("Enter age:"))
+gender=input("Enter gender:")
+city=input("Enter city:")
+college=input("Enter college:")
+cur_year=2026
+birth_year=cur_year-age
+fav_language=input("Enter favourite programming language:")
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Gender: {gender}")
+print(f"City: {city}")
+print(f"College: {college}")
+print(f"Birth Year: {birth_year}")
+print(f"Favorite Programming Language: {fav_language}")
