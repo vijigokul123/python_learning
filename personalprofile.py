edb81c355db7a5426177ac3fbe7446fc8ea85622
Gokul <vijigokul2006@gmail.com>
2026-06-16 15:31:27 +0530

    personalprofile

--- personalprofile.py
+++ personalprofile.py
@@ -7,10 +7,10 @@ cur_year=2026
 birth_year=cur_year-age
 fav_language=input("Enter favourite programming language:")
 
-print(f"Name: {name}")
-print(f"Age: {age}")
-print(f"Gender: {gender}")
-print(f"City: {city}")
-print(f"College: {college}")
-print(f"Birth Year: {birth_year}")
-print(f"Favorite Programming Language: {fav_language}")
+print("Name:", name)
+print("Age:", age)
+print("Gender:", gender)
+print("City:", city)
+print("College:", college)
+print("Birth Year:", birth_year)
+print("Favorite Programming Language:", fav_language)
