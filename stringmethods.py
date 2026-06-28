eff580005082738d35d7ebc4505810b454601d51
Gokul <vijigokul2006@gmail.com>
2026-06-28 11:04:25 +0530

    String_methods

---
+++ stringmethods.py
@@ -0,0 +1,9 @@
+text = "  hello python  "
+
+print(text.upper())
+print(text.lower())
+print(text.title())
+print(text.strip())
+print(text.replace("python", "world"))
+print(text.find("o"))
+print(text.count("o"))
